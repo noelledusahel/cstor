8a9f1843b5c363814284f37298ca9d9351b69bdc
Avril Dunleavy & Noelle Barber & Ryan Landgren & Sarah Dudek <pair+avrilanne.noelledusahel.rklangrin.sarahdudek@devbootcamp.com>
2017-03-08 15:59:20 -0600

    tests associations. All clear. Capybara out

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do
+  User.create!({username: Faker::Internet.user_name, email: Faker::Internet.email, password: "password", admin: false})
+end
+
+3.times do
+  User.create!({username: Faker::Internet.user_name, email: Faker::Internet.email, password: "password", admin: true})
+end
+
+7.times do
+  Resource.create!({title: Faker::Hacker.noun, abstract: Faker::Hacker.say_something_smart, teacher_only: false, teacher_id: Faker::Number.between(6, 8)})
+end
+
+4.times do
+  Resource.create!({title: Faker::Hacker.adjective, abstract: Faker::Hacker.say_something_smart, teacher_only: true, teacher_id: Faker::Number.between(6, 8)})
+end
+
+10.times do
+  Like.create!({liker_id: Faker::Number.between(1, 8), resource_id: Faker::Number.between(1, 11)})
+end
+
+15.times do
+  Tag.create!({name: Faker::Hacker.noun})
+end
+
+ResourceTag.create!({resource_id: 1, tag_id: Faker::Number.between(1, 15)})
+ResourceTag.create!({resource_id: 2, tag_id: Faker::Number.between(1, 15)})
+ResourceTag.create!({resource_id: 3, tag_id: Faker::Number.between(1, 15)})
+ResourceTag.create!({resource_id: 4, tag_id: Faker::Number.between(1, 15)})
+ResourceTag.create!({resource_id: 5, tag_id: Faker::Number.between(1, 15)})
+ResourceTag.create!({resource_id: 6, tag_id: Faker::Number.between(1, 15)})
+ResourceTag.create!({resource_id: 7, tag_id: Faker::Number.between(1, 15)})
+ResourceTag.create!({resource_id: 8, tag_id: Faker::Number.between(1, 15)})
+ResourceTag.create!({resource_id: 9, tag_id: Faker::Number.between(1, 15)})
+ResourceTag.create!({resource_id: 10, tag_id: Faker::Number.between(1, 15)})
+ResourceTag.create!({resource_id: 11, tag_id: Faker::Number.between(1, 15)})
+ResourceTag.create!({resource_id: 11, tag_id: Faker::Number.between(1, 15)})
