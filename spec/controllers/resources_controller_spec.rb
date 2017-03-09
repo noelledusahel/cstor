db03af68200592126044861e700836f0dd91addb
Avril Dunleavy & Noelle Barber <pair+avrilanne.noelledusahel@devbootcamp.com>
2017-03-08 18:35:04 -0600

    Finished with new resource form

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -36,6 +36,35 @@ describe ResourcesController do
       get :show, { id: resource.id }
       expect(response).to render_template(:show)
     end
+    it "renders the :show template" do
+      get :show, { id: resource.id }
+      expect(response).to render_template(:show)
+    end
+
+    describe "GET #new" do
+      it "responds with status code 200" do
+        get :new
+        expect(response).to have_http_status 200
+      end
+
+      it "assigns the newly created resource as @resource" do
+        get :new
+        expect(assigns(:resource)).to be_a_new Resource
+      end
+
+      it "renders the new resourse form" do
+        get :new
+        expect(response).to render_template(:new)
+      end
+    end
+
+    describe "POST #create" do
+      it "creates a Resource object" do
+        post :create, { resource: {title: "Where is N?", abstract: "She is missing!", url: "google.com", teacher_only: false}}
+        expect(assigns(:resource)).to be_a Resource
+      end
   end
+
+end
 
 end
