c48b7903258c52df913bb7a9266939e934ee849f
Noelle Barber <barberossa@gmail.com>
2017-03-09 11:00:15 -0600

    create tag routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
- resources :resources
- root to: "resources#index"
+  resources :resources
 
- resources :users, except: [:index, :update, :edit, :destroy]
+  resources :tags, only: [:new, :create, :show]
 
- resources :sessions, only: [:new, :create, :destroy]
+  root to: "resources#index"
+
+  resources :users, except: [:index, :update, :edit, :destroy]
+
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
