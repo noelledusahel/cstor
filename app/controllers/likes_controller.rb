7917fc9af6f32752f693da04149a1a5ac49a6c1b
Sarah Dudek <email": null>
2017-03-09 20:38:22 -0600

    Fix new resource errors issue, should still address code though

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,17 +3,17 @@ class LikesController < ApplicationController
   end
 
   def create
+    path = URI(request.referer).path
     @like = Like.new(liker_id: current_user.id, resource_id: params[:resource_id])
     if @like.save
-      redirect_to resources_path
+      redirect_to path
     else
       flash[:notice] = "There was an error"
-      redirect_to resources_path
+      redirect_to path
     end
   end
 
   def destroy
-    puts "\n" * 10
     path = URI(request.referer).path
     @like = Like.find_by(liker_id: current_user.id, resource_id: params[:resource_id])
     @like.destroy
